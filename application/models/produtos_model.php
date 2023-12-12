149da2299338fd9d46687707f619cf8a755cc391
Leonardo Gabriel <leonardogabrielsilva1@outlook.com>
2023-12-12 09:19:15 -0300

    Update vendas

--- application/models/produtos_model.php
+++ application/models/produtos_model.php
@@ -2,6 +2,7 @@
 class Produtos_model extends CI_Model {
 
     public function buscarTodos(){
+        $this->db->where("vendido", false);
         return $this->db->get("produtos")->result_array();
 
     }
--- application/models/vendas_model.php
+++ application/models/vendas_model.php
@@ -1,7 +1,12 @@
 <?php
 class Vendas_model extends CI_Model {
 
-    public function salva ($venda){
-        return $this->db->insert("vendas", $venda);
+    public function salva($venda) {
+        $this->db->insert("vendas", $venda);
+    
+        // Atualiza o status do produto para "vendido" na tabela de produtos
+        $dados_atualizados = array("vendido" => 1);
+        $this->db->where("id", $venda["produto_id"]);
+        $this->db->update("produtos", $dados_atualizados);
     }
 }
